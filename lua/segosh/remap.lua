32dacf25924576cbcf96f7c62f48490b045da3c6
Boris Lokotkov <woweuro2010@gmail.com>
2023-04-17 16:43:26 +0300

    add splits keymap

--- lua/segosh/remap.lua
+++ lua/segosh/remap.lua
@@ -35,3 +35,5 @@ vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><
 vim.keymap.set("n", "<leader><leader>", function()
     vim.cmd("so")
 end)
+
+vim.keymap.set("n", "<Tab>", "<C-w>")
