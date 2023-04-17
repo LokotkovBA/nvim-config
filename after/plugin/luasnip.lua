216aa3914cc3ef5b9cf07cdd69a2a1b40fcd3cb1
Boris Lokotkov <woweuro2010@gmail.com>
2023-04-17 15:32:15 +0300

    add snippets for react

---
+++ after/plugin/luasnip.lua
@@ -0,0 +1,38 @@
+local ls = require("luasnip")
+local types = require("luasnip.util.types")
+ls.config.set_config({
+    -- This tells ls to remember to keep around the last snippet.
+    -- You can jump back into it even if you move outside of the selection
+    history = true,
+    -- This one is cool cause if you have dynamic snippets, it updates as you type!
+    updateevents = "TextChanged,TextChangedI",
+    -- Autosnippets:
+    enable_autosnippets = true,
+    ext_opts = {
+        [types.choiceNode] = {
+            active = {
+                virt_text = { { " « ", "NonTest" } },
+            },
+        },
+    },
+})
+
+require("luasnip.loaders.from_vscode").load({ paths = { "~/AppData/Local/nvim/after/plugin/my-snippets" } })
+vim.keymap.set({ "i", "s" }, "<c-k>", function()
+    if ls.expand_or_jumpable() then
+        ls.expand_or_jump()
+    end
+end, { silent = true })
+vim.keymap.set({ "i", "s" }, "<c-j>", function()
+    if ls.jumpable(-1) then
+        ls.jump(-1)
+    end
+end, { silent = true })
+
+vim.keymap.set({ "i", "s" }, "<c-l>", function()
+    if ls.choice_active() then
+        ls.change_choice(1)
+    end
+end, { silent = true })
+
+vim.keymap.set("n", "<leader><leader>s", "<cmd>source ~/AppData/Local/nvim/after/plugin/luasnip.lua<CR>")
