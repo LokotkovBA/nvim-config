19b77afa876c4ef31a8f5257f98101c4900a1ee9
Boris Lokotkov <woweuro2010@gmail.com>
2023-04-18 16:12:42 +0300

    change cmp mappings

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -15,9 +15,14 @@ local cmp = require("cmp")
 local cmp_select = { behavior = cmp.SelectBehavior.Select }
 local cmp_mappings = lsp.defaults.cmp_mappings({
     ["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
-    ["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
+    ["<C-n>"] = cmp.mapping(function()
+        if cmp.visible() then
+            cmp.select_next_item()
+        else
+            cmp.complete()
+        end
+    end, { 'i' }),
     ["<C-y>"] = cmp.mapping.confirm({ select = true }),
-    ["<Tab>"] = cmp.mapping.complete(),
 })
 
 lsp.setup_nvim_cmp({
