05101478264c383d5b55a6198b307c8b86316ee5
LokotkovBA <woweuro2010@gmail.com>
2023-05-01 17:58:14 +0300

    small editor change

--- lua/segosh/set.lua
+++ lua/segosh/set.lua
@@ -8,7 +8,7 @@ vim.opt.expandtab = true
 
 vim.opt.smartindent = true
 
-vim.opt.wrap = false
+vim.opt.wrap = true
 
 vim.opt.swapfile = false
 vim.opt.backup = false
@@ -20,7 +20,7 @@ vim.opt.incsearch = true
 
 vim.opt.termguicolors = true
 
-vim.opt.scrolloff = 8
+vim.opt.scrolloff = 16
 vim.opt.signcolumn = "yes"
 vim.opt.isfname:append("@-@")
 
