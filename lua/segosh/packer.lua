4736dee570fbf1ff9494d7a13141b8deb8c43ec0
LokotkovBA <woweuro2010@gmail.com>
2023-05-03 14:30:58 +0300

    add nvim-surround

--- lua/segosh/packer.lua
+++ lua/segosh/packer.lua
@@ -5,12 +5,13 @@ vim.cmd [[packadd packer.nvim]];
 
 return require('packer').startup(function(use)
     -- Packer can manage itself
-    use 'wbthomason/packer.nvim';
-    use {
-        'nvim-telescope/telescope.nvim', tag = '0.1.1',
+    use('wbthomason/packer.nvim');
+    use({
+        'nvim-telescope/telescope.nvim',
+        tag = '0.1.1',
         -- or                            , branch = '0.1.x',
         requires = { { 'nvim-lua/plenary.nvim' } }
-    }
+    })
     use("nvim-telescope/telescope-file-browser.nvim")
     use('AstroNvim/astrotheme')
     use({
@@ -31,7 +32,7 @@ return require('packer').startup(function(use)
     use('ThePrimeagen/harpoon')
     use("theprimeagen/refactoring.nvim")
     use('mbbill/undotree')
-    use {
+    use({
         'VonHeikemen/lsp-zero.nvim',
         branch = 'v2.x',
         requires = {
@@ -51,7 +52,7 @@ return require('packer').startup(function(use)
             { 'hrsh7th/cmp-nvim-lsp' }, -- Required
             { 'L3MON4D3/LuaSnip' },     -- Required
         }
-    }
+    })
     use({
         "windwp/nvim-ts-autotag",
         config = function()
@@ -62,20 +63,20 @@ return require('packer').startup(function(use)
     use("hrsh7th/cmp-buffer")
     use("hrsh7th/cmp-nvim-lua")
     use("saadparwaiz1/cmp_luasnip")
-    use {
+    use({
         "windwp/nvim-autopairs",
         config = function() require("nvim-autopairs").setup {} end
-    }
-    use {
+    })
+    use({
         'filipdutescu/renamer.nvim',
         branch = 'master',
         requires = { { 'nvim-lua/plenary.nvim' } }
-    }
+    })
     use("numToStr/Comment.nvim")
     use("kyazdani42/nvim-web-devicons")
     use("nvim-lua/plenary.nvim")
     use("MunifTanjim/nui.nvim")
-    use {
+    use({
         "nvim-neo-tree/neo-tree.nvim",
         branch = "v2.x",
         requires = {
@@ -83,13 +84,22 @@ return require('packer').startup(function(use)
             "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
             "MunifTanjim/nui.nvim",
         }
-    }
+    })
     use("lewis6991/gitsigns.nvim")
-    use {
+    use({
         'nvim-lualine/lualine.nvim',
         requires = { 'nvim-tree/nvim-web-devicons', opt = true }
-    }
+    })
     use("tpope/vim-fugitive")
     use("jose-elias-alvarez/null-ls.nvim")
     use("MunifTanjim/prettier.nvim")
+    use({
+        "kylechui/nvim-surround",
+        tag = "*", -- Use for stability; omit to use `main` branch for the latest features
+        config = function()
+            require("nvim-surround").setup({
+                -- Configuration here, or leave empty to use defaults
+            })
+        end
+    })
 end)
