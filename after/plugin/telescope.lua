ef54ab57b47ffacc17db20b0d514864558b2eed7
LokotkovBA <woweuro2010@gmail.com>
2023-05-22 23:45:48 +0300

    change telescope mappings

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -4,38 +4,4 @@ vim.keymap.set('n', '<C-p>', builtin.git_files, {});
 vim.keymap.set('n', '<leader>ps', function()
     builtin.grep_string({ str = vim.fn.input("Grep > ") });
 end);
-
-local telescope = require("telescope")
-local function telescope_buffer_dir()
-    return vim.fn.expand('%:p:h')
-end
-local actions = require("telescope.actions")
-telescope.setup {
-    defaults = {
-        mappings = {
-            n = {
-                ["q"] = actions.close
-            },
-        },
-    },
-    extensions = {
-        file_browser = {
-            theme = "dropdown",
-        },
-    },
-}
-
-telescope.load_extension("file_browser")
-
-vim.keymap.set("n", "<C-f>", function()
-    telescope.extensions.file_browser.file_browser({
-        path = "%:p:h",
-        cwd = telescope_buffer_dir(),
-        respect_gitignore = false,
-        hidden = true,
-        grouped = true,
-        previewer = false,
-        initial_mode = "normal",
-        layout_config = { height = 40 }
-    })
-end)
+vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
