5fe679af389ae9b8f3d077281a4a67b744c16f31
LokotkovBA <woweuro2010@gmail.com>
2023-05-23 00:12:26 +0300

    fix telescope's grep_string

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -2,6 +2,13 @@ local builtin = require('telescope.builtin');
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {});
 vim.keymap.set('n', '<C-p>', builtin.git_files, {});
 vim.keymap.set('n', '<leader>ps', function()
-    builtin.grep_string({ str = vim.fn.input("Grep > ") });
+    vim.ui.input({ prompt = "Grep > " },
+        function(input)
+            if not (input and #input > 0) then
+                return
+            end
+            builtin.grep_string({ search = input });
+        end
+    )
 end);
 vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
