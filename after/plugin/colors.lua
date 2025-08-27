a92d3b798484f18f07b7fffb4b3b3a6eeeff70ab
 Boris Lokotkov <woweuro2010@gmail.com>
2025-08-27 12:04:15 +0300

    feat(color): transparent background

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,6 +1,11 @@
 function SetTheme(color)
     color = color or "tokyonight-night"
     vim.cmd.colorscheme(color)
+
+    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NeoTreeNormal", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NeoTreeNormalNC", { bg = "none" })
 end
 
 SetTheme()
