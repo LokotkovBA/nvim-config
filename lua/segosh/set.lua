61d71a117764ccea6bd2a2a718e602d30778996f
 Boris Lokotkov <woweuro2010@gmail.com>
2025-08-27 12:16:43 +0300

    feat(set): different tabstop and shiftwith for work

--- lua/segosh/set.lua
+++ lua/segosh/set.lua
@@ -4,6 +4,16 @@ vim.opt.relativenumber = true
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
+
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+	pattern = "~/work/*",
+	callback = function()
+		vim.opt_local.tabstop = 2
+		vim.opt_local.softtabstop = 2
+		vim.opt_local.shiftwidth = 2
+	end,
+})
+
 vim.opt.expandtab = true
 
 vim.opt.smartindent = true
