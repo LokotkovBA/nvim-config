96fbac92590672c9bfdacc89008ba20c1d80a78f
 Boris Lokotkov <woweuro2010@gmail.com>
2025-08-27 15:08:59 +0300

    feat: transparent bacgrkound

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -2,10 +2,13 @@ function SetTheme(color)
     color = color or "tokyonight-night"
     vim.cmd.colorscheme(color)
 
+
     vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
     vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
+
     vim.api.nvim_set_hl(0, "NeoTreeNormal", { bg = "none" })
     vim.api.nvim_set_hl(0, "NeoTreeNormalNC", { bg = "none" })
-end
 
-SetTheme()
+    vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
+end
