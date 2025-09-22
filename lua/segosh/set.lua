6e575c44c72b6e5ea94de6cd2b18710029aaff24
 Boris Lokotkov <woweuro2010@gmail.com>
2025-09-22 15:42:48 +0300

    fix(set): indentation inside work

--- lua/segosh/set.lua
+++ lua/segosh/set.lua
@@ -1,17 +1,21 @@
-vim.opt.nu = true
+vim.opt.number = true
 vim.opt.relativenumber = true
 
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 
+local home = os.getenv("HOME")
+
+vim.api.nvim_create_augroup("IndentWork", { clear = true })
 vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
-	pattern = "~/work/*",
-	callback = function()
-		vim.opt_local.tabstop = 2
-		vim.opt_local.softtabstop = 2
-		vim.opt_local.shiftwidth = 2
-	end,
+    group = "IndentWork",
+    pattern = home .. "/work/*",
+    callback = function()
+        vim.opt_local.tabstop = 2
+        vim.opt_local.softtabstop = 2
+        vim.opt_local.shiftwidth = 2
+    end,
 })
 
 vim.opt.expandtab = true
@@ -22,7 +26,7 @@ vim.opt.wrap = true
 
 vim.opt.swapfile = false
 vim.opt.backup = false
-vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undodir = home .. "/.vim/undodir"
 vim.opt.undofile = true
 
 vim.opt.hlsearch = false
